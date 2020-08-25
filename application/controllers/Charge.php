c1889f64a97f24ad0dbf8c2fa04b5b63ef77da44
Della Emilia <dellaemiliaa@gmail.com>
2020-08-25 15:18:01 +0700

    update cart payment

--- application/controllers/Charge.php
+++ application/controllers/Charge.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cart extends CI_Controller {
+class Charge extends CI_Controller {
 
     public function index() {
         var_dump($_POST);
