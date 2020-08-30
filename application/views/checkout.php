7f79fd41d9f165b03adb6cb1b431f099b72c8f77
denifrahman <denifrahman@gmail.com>
2020-08-30 09:08:27 +0700

    test

--- application/views/checkout.php
+++ application/views/checkout.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");?>
 <!-- 
 Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.7
 Version: 4.7.5
@@ -16,103 +19,157 @@ License: You must have a valid license purchased only from themeforest(the above
 <!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
-    <!--<![endif]-->
-    <!-- BEGIN HEAD -->
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
 
-    <head>
-        <meta charset="utf-8" />
-        <title>Metronic Admin Theme #1 | 500 Page Option 2</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content="Preview page of Metronic Admin Theme #1 for 500 page option 2" name="description" />
-        <meta content="" name="author" />
-        <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
-        <!-- END GLOBAL MANDATORY STYLES -->
-        <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
-        <!-- END THEME GLOBAL STYLES -->
-        <!-- BEGIN PAGE LEVEL STYLES -->
-        <link href="../assets/pages/css/error.min.css" rel="stylesheet" type="text/css" />
-        <!-- END PAGE LEVEL STYLES -->
-        <!-- BEGIN THEME LAYOUT STYLES -->
-        <!-- END THEME LAYOUT STYLES -->
-        <link rel="shortcut icon" href="favicon.ico" /> </head>
-    <!-- END HEAD -->
+<head>
+	<meta charset="utf-8" />
+	<title>Metronic Admin Theme #1 | 500 Page Option 2</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<meta content="Preview page of Metronic Admin Theme #1 for 500 page option 2" name="description" />
+	<meta content="" name="author" />
+	<!-- BEGIN GLOBAL MANDATORY STYLES -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+	<link href="assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+	<!-- END GLOBAL MANDATORY STYLES -->
+	<!-- BEGIN THEME GLOBAL STYLES -->
+	<link href="assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+	<link href="assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+	<!-- END THEME GLOBAL STYLES -->
+	<!-- BEGIN PAGE LEVEL STYLES -->
+	<link href="assets/pages/css/error.min.css" rel="stylesheet" type="text/css" />
+	<!-- END PAGE LEVEL STYLES -->
+	<!-- BEGIN THEME LAYOUT STYLES -->
+	<!-- END THEME LAYOUT STYLES -->
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<!-- END HEAD -->
 
-    <body class=" page-500-full-page">
-		<div class="row" style="justify-content: center;">
+<body class=" page-500-full-page">
+	<div class="row" style="justify-content: center;">
 		<div class="col-md-12">
-                <div class=" details">
-					<center><h2>Silahkan Tunggu<h2></center>
-                </div>
-            </div>
-            <div class="col-md-12 page-500" style="margin-top: 30%">
-                <div class=" details">
-				<img src="assets/img/2910750.png" alt="Girl in a jacket" width="100%">
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class=" details">
+			<div class=" details">
 				<center>
-					<h2>Tunggu ...</h2>
+					<h2>Silahkan Tunggu<h2>
+				</center>
+			</div>
+		</div>
+		<div class="col-md-12 page-500" style="margin-top: 30%">
+			<div class=" details">
+				<img src="assets/img/2910750.png" alt="Girl in a jacket" width="100%">
+			</div>
+		</div>
+		<div class="col-md-12">
+			<div class=" details">
+				<center>
+					<h2 onclick="getStatusTransaksi()">Tunggu ...</h2>
 					<p style="color: grey;">Sampai anda akan di arahkan ke invoice.</p>
 				</center>
-                </div>
-            </div>
-        </div>
-        <!--[if lt IE 9]>
-<script src="../assets/global/plugins/respond.min.js"></script>
-<script src="../assets/global/plugins/excanvas.min.js"></script> 
-<script src="../assets/global/plugins/ie8.fix.min.js"></script> 
+			</div>
+		</div>
+	</div>
+	<!--[if lt IE 9]>
+<script src="assets/global/plugins/respond.min.js"></script>
+<script src="assets/global/plugins/excanvas.min.js"></script> 
+<script src="assets/global/plugins/ie8.fix.min.js"></script> 
 <![endif]-->
-        <!-- BEGIN CORE PLUGINS -->
-        <script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-        <script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
-        <!-- END CORE PLUGINS -->
-        <!-- BEGIN THEME GLOBAL SCRIPTS -->
-        <script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
-        <!-- END THEME GLOBAL SCRIPTS -->
-        <!-- BEGIN THEME LAYOUT SCRIPTS -->
-        <!-- END THEME LAYOUT SCRIPTS -->
-        <script>
-            $(document).ready(function()
-            {
-                $('#clickmewow').click(function()
-                {
-                    $('#radio1003').attr('checked', 'checked');
-                });
-            })
-        </script>
-    <!-- Google Code for Universal Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-37564768-1', 'auto');
-  ga('send', 'pageview');
-</script>
-<!-- End -->
+	<!-- BEGIN CORE PLUGINS -->
+	<script src="assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+	<script src="assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+	<script src="assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+	<script src="assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+	<!-- END CORE PLUGINS -->
+	<!-- BEGIN THEME GLOBAL SCRIPTS -->
+	<script src="assets/global/scripts/app.min.js" type="text/javascript"></script>
+	<!-- END THEME GLOBAL SCRIPTS -->
+	<!-- BEGIN THEME LAYOUT SCRIPTS -->
+	<!-- END THEME LAYOUT SCRIPTS -->
+	<script>
+		$(document).ready(function() {
+			$('#clickmewow').click(function() {
+				$('#radio1003').attr('checked', 'checked');
+			});
+		});
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W276BJ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W276BJ');</script>
-<!-- End -->
+		function getStatusTransaksi() {
+			var order_id = getUrlVars(); // ORDER-105-1598750272
+			$.ajax({
+				type: "GET",
+				url: "https://api.midtrans.com/v2/"+order_id[order_id]+"/status",
+				headers: {
+					'Authorization': 'Basic TWlkLXNlcnZlci1BRm5oenFPWlZaY2hQSVB5TkYzQ29xcUM6bV9CYW5ndW4yMDIw',
+					'Accept': 'application/json',
+					"Access-Control-Allow-Origin":"*",
+					"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+
+				},
+				crossDomain: true,
+				// dataType: "jsonp",
+				success: function(f) {
+					$("#kota").append('<option value="" selected>Pilih Kota... </option>');
+					Print.postMessage(JSON.stringify(f[0]));
+					$.each(f, function(bb) {
+						$("#kota").append('<option  value="' + f[bb].city_id + '">' + f[bb].city_name + '  </option>');
+					});
+				}
+			});
+		}
+
+		function getUrlVars() {
+			var vars = [],
+				hash;
+			var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+			for (var i = 0; i < hashes.length; i++) {
+				hash = hashes[i].split('=');
+				vars.push(hash[0]);
+				vars[hash[0]] = hash[1];
+			}
+			return vars;
+		}
+	</script>
+	<!-- Google Code for Universal Analytics -->
+	<script>
+		(function(i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function() {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+		ga('create', 'UA-37564768-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- End -->
+
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W276BJ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'//www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-W276BJ');
+	</script>
+	<!-- End -->
 </body>
 
 
